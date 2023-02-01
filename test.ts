22b6f72548fd7e07b6907d78dc291a2f1f6ed8ec
natsukilab <erocostv@gmail.com>
2023-02-01 20:49:38 +0900

    change async function

--- test.ts
+++ test.ts
@@ -4,6 +4,16 @@ var api = {
  affiliateid: 'utamita-992'
 }
 var fanza = new FanzaAPI(api);
-fanza.getMp4URL(`oreco169`).then((mp4) => {
-    console.log(mp4);
-})
+/*
+const mp4_url = await fanza.getMp4URL(`oreco169`);
+console.log(mp4_url);
+*/
+var requestOptions = {
+    site:'FANZA',
+    service:'digital',
+    floor:'videoc',
+    article:'genre',
+    article_id:'1018'
+}
+var data = await fanza.ItemList(requestOptions);
+console.log(data);
