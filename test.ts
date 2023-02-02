4e5afd0dbec8546405e144fbc788419b6d51c35e
natsukilab <erocostv@gmail.com>
2023-02-02 16:45:43 +0900

    getMp4URL修正

--- test.ts
+++ test.ts
@@ -1,15 +1,14 @@
 import { FanzaAPI } from "./mod.ts";
 var api = {
-    apiid: '',
-    affiliateid: ''
+    apiid: 'bNKvmKPuTDunaEwC6GAP',
+    affiliateid: 'utamita-992'
 }
 var fanza = new FanzaAPI(api);
-var requestOptions = {
+var requestOptions: string[] = {
     site:'FANZA',
     service:'digital',
-    floor:'videoc',
-    article:'genre',
-    article_id:'1018'
+    cid:`smus014`
 }
 var data: string[] = await fanza.ItemList(requestOptions);
-console.log(data);
+var mp4: string = await fanza.getMp4URL(data.result.items[0].product_id);
+console.log(mp4)
